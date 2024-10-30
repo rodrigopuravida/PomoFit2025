1be25d66a3eadbe8a58b561dabf8234c308e32a9
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-10-29 20:53:07 -0600

    Adding intital timer and some images

--- PomoFit/ContentView.swift
+++ PomoFit/ContentView.swift
@@ -8,15 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+   
+    @State private var timeRemaining = 30
+    let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("1024")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 25))
+                .padding()
+            
+            Text("Time: \(timeRemaining)")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+                .background(.black.opacity(0.75))
+                .clipShape(.capsule)
         }
-        .padding()
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+        }
+        
     }
+    
+    
 }
 
 #Preview {
